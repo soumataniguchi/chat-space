25b8f575dbd37c27e73226d268bce4311913f46e
soumataniguchi <staniguchi824@gmail.com>
2020-03-23 02:11:42 +0900

    インクリメンタルサーチ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")
+		return nil if params[:keyword] == ""
+		@users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
 		respond_to do |format|
 			format.html
 			format.json
 		end
 	end
-
+	
 	def edit
 	end
 
